d6b2d9ce6e530cc193ad6360b95883a1cd437d5f
iadityasingh-3616 <iadityasingh2468@gmail.com>
2024-11-12 14:50:50 +0530

    first commit

---
+++ JS DAY 4/script.js
@@ -0,0 +1,43 @@
+// array method
+
+let number = [2, 4, 6, 5, 7, 8]
+console.log(number.length)
+
+// push (insert a value in last position of array )
+
+
+// number.push(10)
+// number.push(13)
+// number.push(15)
+// number.push(18)
+// number.push(19)
+// console.log(number)
+// console.log(number.length)
+
+//pop method ( remove the value from last position)
+
+// number.pop()
+// number.pop()
+// number.pop()
+// number.pop()
+// console.log(number)
+
+//shift  ( remove the value from first position of array)
+// number.shift()
+// console.log(number)
+
+//unshift ( insert the value in first position of array)
+
+// number.unshift(0)
+// console.log(number)
+
+// to string
+
+// let convert = number.toString()
+// console.log(typeof (convert))
+// console.log(number.toString())
+
+// console.log(number.at(4))
+
+// console.log(number.join('www.'))
+
